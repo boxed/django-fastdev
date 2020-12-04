663012cd971b58d18684986ce2e0b0998584d6ff
Anders Hovmöller <anders.hovmoller@trioptima.com>
2020-12-04 10:42:17 +0100

    Turn off checks for runserver

--- django_fastdev/__init__.py
+++ django_fastdev/__init__.py
@@ -2,3 +2,8 @@ __version__ = '1.0.2'
 
 
 default_app_config = 'django_fastdev.django_app.FastDevConfig'
+
+
+from django.core.management.commands.runserver import Command
+Command.check = lambda *_, **__: print('Skipped model validations')
+Command.check_migrations = lambda *_, **__: print('Skipped migrations checks')
